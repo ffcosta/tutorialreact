508a022622e759d56e61c2758faf208516b37c19
Fernando F Costa <ffcostaa@outlook.com>
2019-05-12 20:48:41 -0300

    create deploy

---
+++ build/precache-manifest.4b239cc328e41b372cea21b60f41b228.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab34573c02c4c38552bf0437e43138f7",
+    "url": "/tutorialreact/index.html"
+  },
+  {
+    "revision": "d9875b4f177bbdf15cea",
+    "url": "/tutorialreact/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "14f00e6937bc8cc25f22",
+    "url": "/tutorialreact/static/css/main.7c5166e0.chunk.css"
+  },
+  {
+    "revision": "d9875b4f177bbdf15cea",
+    "url": "/tutorialreact/static/js/2.4b2da874.chunk.js"
+  },
+  {
+    "revision": "14f00e6937bc8cc25f22",
+    "url": "/tutorialreact/static/js/main.0b442ab3.chunk.js"
+  },
+  {
+    "revision": "5192a7f5a8c0449ccd47",
+    "url": "/tutorialreact/static/js/runtime~main.85f4ae82.js"
+  }
+]);
